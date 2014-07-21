9f29a64c54f1250c0676d315ed4d2ecead4fbc1f
Tim Jones <tim.jones@jhspecialty.com>
2014-07-21 07:21:14 -0400

    Initial Setup
    Consolidating all other code to a single repo.

---
+++ App.h
@@ -0,0 +1,63 @@
+//==============================================================================
+/*
+	Primary application class
+
+	3/11/2014
+	SDLTutorials.com
+	Tim Jones
+*/
+//==============================================================================
+#ifndef __APP_H__
+	#define __APP_H__
+
+#include <SDL2/SDL.h>
+
+#include "TextureBank.h"
+
+class App {
+	private:
+		static App Instance;
+
+		bool Running = true;
+
+		SDL_Window* Window = NULL;
+		SDL_Renderer* Renderer = NULL;
+		SDL_Surface* PrimarySurface = NULL;
+
+		static const int WindowWidth = 1024;
+		static const int WindowHeight = 768;
+
+		Texture* TestTexture;
+
+	private:
+		App();
+
+		// Capture SDL Events
+		void OnEvent(SDL_Event* Event);
+
+		// Initialize our SDL game / app
+		bool Init();
+
+		// Logic loop
+		void Loop();
+
+		// Render loop (draw)
+		void Render();
+
+		// Free up resources
+		void Cleanup();
+
+	public:
+		int Execute(int argc, char* argv[]);
+
+	public:
+		SDL_Renderer* GetRenderer();
+
+	public:
+		static App* GetInstance();
+
+		static int GetWindowWidth();
+		static int GetWindowHeight();
+};
+
+#endif
